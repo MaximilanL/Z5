bcde233bd00f79ab65710139821c434da6439e72
MaximilanL <maxpuzatik@gmail.com>
2018-09-20 23:12:32 +0300

    zs

--- Index.php
+++ Index.php
@@ -10,37 +10,64 @@ $packages = [
         "dependencies" => ["B", "D"]],
 
     "D" => ["name" => "D",
-        "dependencies" => []]
+        "dependencies" => ["A"]]
 ];
 
-function validatePackageDefinitions(array $packages):void{
+function validatePackageDefinitions(array $packages): void
+{
 
     foreach ($packages as $k => $v) {
-        if($v["name"]===$k){
+        if ($v["name"] === $k) {
             echo "true<br>";
-        }
-        else{
+        } else {
             echo "false<br>";
         }
 
-        if(isset($v["dependencies"])){
+        if (isset($v["dependencies"])) {
             echo "true<br>";
-        }
-        else{
+        } else {
             echo "false<br>";
         }
 
-        foreach ($v["dependencies"] as $i){
-            if($i=="A" or $i=="B" or $i=="C" or $i=="D" or empty($i)){
+        foreach ($v["dependencies"] as $i) {
+            if ($i == "A" or $i == "B" or $i == "C" or $i == "D" or empty($i)) {
                 echo "true<br>";
-            }
-            else{
+            } else {
                 echo "false<br>";
             }
         }
+    }
+
+
+}
+
+function getAllPackageDependencies(array $packages, string $packageName): array
+{
+    $zav = [];
+    foreach ($packages as $x => $z) {
+        if ($x === $packageName) {
+            $zav += $z["dependencies"];
+            continue;
+        }
+
+        if (in_array($zav[0], $z["dependencies"]) or in_array($zav[1], $z["dependencies"]) or in_array($packageName, $z["dependencies"])) {
+            $zav = array_merge($zav, $z["dependencies"]);
         }
 
 
-};
+    }
+
+
+    $result = array_values(array_unique($zav));
+    print_r($result);
+
+
+    if (in_array($packageName, $result)){
+        echo 'Есть циклические зависимости';
+    }
+    return $result;
+}
+
+getAllPackageDependencies($packages, "A");
 validatePackageDefinitions($packages);
 
